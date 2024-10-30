7320d4778e87a061c09010d5d6bf2e4bba15f5db
Фируза <ferika2525@gmail.com>
2024-10-30 14:18:02 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -9,3 +9,16 @@ def password(number):
 number = int(input("Введите ваше число: "))
 result = password(number)
 print(result)
+
+#Добрый день. Решили практически верно, но нужно сохранить подходящие пары в виде строки. Дорабатывая ваш код, можно получить
+
+#def generate_password(n):
+#result = ""
+#for i in range(1, n):
+    #for j in range(i + 1, n):
+        #if (i + j) <= n and n % (i + j) == 0:
+            #result += str(i) + str(j)
+        #return result
+#n = int(input("Введите число от 3 до 20: "))
+#password = generate_password(n)
+#print("Пароль:", password)
